6e17b7305e5907da970be79bfe83b053401a6230
Amy Knudson <amylknudson@gmail.com>
2018-07-09 17:04:29 -0600

    sql schema added

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Users (
+    user_id serial PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    address VARCHAR(100),
+    city VARCHAR(80),
+    state VARCHAR(2),
+    zip INTEGER,
+    phone VARCHAR(24),
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE Properties (
+    property_id serial PRIMARY KEY,
+    address VARCHAR(100),
+    city VARCHAR(80),
+    state VARCHAR(2),
+    zip INTEGER,
+    img TEXT,
+    rent DECIMAL,
+    user_id INTEGER REFERENCES Users (user_id)
+);
