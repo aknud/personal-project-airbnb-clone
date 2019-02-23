754b7480758ed5ac2eb465b63087bb30bdfecc28
Amy Knudson <amylknudson@gmail.com>
2019-02-22 17:31:15 -0700

    update npm packages and fixed nav margins

--- server/controller.js
+++ server/controller.js
@@ -37,7 +37,7 @@ module.exports = {
 	},
 	getAllListings: (req, res) => {
 		const dbi = req.app.get('db');
-		dbi.get_all_listings().then((listings) => res.status(200).send(listings)).catch((err) => {
+		dbi.get_All_Listings().then((listings) => res.status(200).send(listings)).catch((err) => {
 			res.status(500).send({ errorMessage: `Something is broken.` });
 			console.log(err);
 		});
